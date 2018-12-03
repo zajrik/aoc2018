c2d3a85871ba96fec3972589a855e9081718c71c
Zack Campbell <zajrik@gmail.com>
2018-12-03 00:52:44 -0600

    Add day 3 part 1

---
+++ 03/part1.nim
@@ -0,0 +1,42 @@
+import sugar, strformat
+from strutils import splitLines, parseInt, split
+from seqUtils import map, filter
+
+type
+    FabricSection = object
+        id: int
+        left: int
+        top: int
+        width: int
+        height: int
+
+proc withinBounds(f: FabricSection, x: int, y: int): bool =
+    x >= f.top and 
+        y >= f.left and
+        x < f.top + f.height and
+        y < f.left + f.width
+        
+var
+    total: int = 0
+    input: seq[FabricSection] = readFile("input.txt")
+        .splitLines
+        .map(a => a.split(" "))
+        .map(a => a.filter(b => b != "@"))
+        .map(a => FabricSection(
+            id: parseInt($a[0][1..^1]),
+            left: parseInt(a[1].split(',')[0]),
+            top: parseInt(a[1].split(',')[1][0..^2]),
+            width: parseInt(a[2].split('x')[0]),
+            height: parseInt(a[2].split('x')[1])))
+
+proc numberOfIntersectsAtPoint(x: int, y: int): int =
+    for square in input:
+        if square.withinBounds(x, y):
+            result += 1
+
+for i in 0..999:
+    for j in 0..999:
+        if numberOfIntersectsAtPoint(i, j) >= 2:
+            total += 1
+
+echo total
