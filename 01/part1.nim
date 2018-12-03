b7fc48dee64306ec0ce64caefb38c0e9250c074d
Zack Campbell <zajrik@gmail.com>
2018-12-02 23:07:44 -0600

    Simplify day 1 part 1
    I'm learning wooo

--- 01/part1.nim
+++ 01/part1.nim
@@ -1,16 +1,7 @@
-from strutils import parseInt
+from strutils import parseInt, splitLines
+from sequtils import map, foldl
 
-var
-    input: File
-    frequency: int = 0
-
-discard open(input, "input.txt")
-
-while true:
-    if endOfFile(input):
-        close(input)
-        break
-
-    frequency += parseInt(readLine(input))
-
-echo frequency
+echo readFile("input.txt")
+    .splitLines
+    .map(parseInt)
+    .foldl(a + b)
